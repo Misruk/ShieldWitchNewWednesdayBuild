4cd77fd23ee07a08d192685e388da40b6d6f3e39
Tevin Meredith <tevin.meredith@gmail.com>
2016-04-21 22:49:35 -0400

    Updated Menu, camshake script, and various fixes.

--- Shield Witch VS/Assets/MenuManager.cs
+++ Shield Witch VS/Assets/MenuManager.cs
@@ -4,16 +4,87 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
 
-    public static int characterSelected;
+    public static int characterSelected = 0;
+    public GameObject otherMenu;
+    public GameObject mainMenu;
+    public GameObject mainMenuWiz;
+    public GameObject someMenu;
+
+    public GameObject witchAnim;
+    public GameObject wizardAnim;
+
+    void Awake()
+    {
+        mainMenu.SetActive(true);
+        mainMenuWiz.SetActive(false);
+        otherMenu.SetActive(false);
+
+
+        wizardAnim.SetActive(false);
+        witchAnim.SetActive(true);
+    }
+
+    public void LoadLab()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadForest()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void LoadCity()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void MenuLevel()
+    {
+        mainMenu.SetActive(false);
+        otherMenu.SetActive(true);
+    }
+
+    public void MenuLevelWiz()
+    {
+        mainMenuWiz.SetActive(false);
+        someMenu.SetActive(true);
+    }
+
+    public void ReturnToMenu()
+    {
+        mainMenu.SetActive(true);
+        otherMenu.SetActive(false);
+    }
+
+    public void ReturnToMenuWiz()
+    {
+        mainMenuWiz.SetActive(true);
+        someMenu.SetActive(false);
+    }
 
     public void CharacterSelectWitch()
     {
+        Debug.Log("Witch Selected");
         characterSelected = 0;
+
+        mainMenu.SetActive(true);
+        mainMenuWiz.SetActive(false);
+
+        witchAnim.SetActive(true);
+        wizardAnim.SetActive(false);
     }
 
     public void CharacterSelectWizard()
     {
+        Debug.Log("Wizard Selected");
         characterSelected = 1;
+
+        mainMenuWiz.SetActive(true);
+        mainMenu.SetActive(false);
+
+        wizardAnim.SetActive(true);
+        witchAnim.SetActive(false);
     }
 
     public void StartGame()
--- Shield Witch VS/Assets/Scripts/CamShakeSimple.cs
+++ Shield Witch VS/Assets/Scripts/CamShakeSimple.cs
@@ -5,18 +5,46 @@ public class CamShakeSimple : MonoBehaviour
 {
 
     Vector3 originalCameraPosition;
+    public GameObject character1;
+    //public GameObject character2;
 
     float shakeAmt = 0;
 
+    bool used = false;
+
     public Camera mainCamera;
+
+    void Start()
+    {
+       // character1 = GameObject.Find("Player_Test");
+        character1 = GetComponent<GameObject>();
+        //character2 = GetComponent<GameObject>();
+    }
+    
+    void Update()
+    {
+        if(used == true)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 
     void OnCollisionEnter2D(Collision2D coll)
     {
+        if (coll.gameObject.tag == "Player")
+        {
+            shakeAmt = coll.relativeVelocity.magnitude * .0025f;
+            shakeAmt = 25 * .0025f;
+            InvokeRepeating("CameraShake", 0, .01f);
+            Invoke("StopShaking", 0.3f);
+            
+        }
 
-        shakeAmt = coll.relativeVelocity.magnitude * .0025f;
-        InvokeRepeating("CameraShake", 0, .01f);
-        Invoke("StopShaking", 0.3f);
+    }
 
+    void Destroy()
+    {
+        Destroy(this.gameObject);
     }
 
     void CameraShake()
@@ -26,14 +54,34 @@ public class CamShakeSimple : MonoBehaviour
             float quakeAmt = Random.value * shakeAmt * 2 - shakeAmt;
             Vector3 pp = mainCamera.transform.position;
             pp.y += quakeAmt; // can also add to x and/or z
+            pp.x += quakeAmt;
             mainCamera.transform.position = pp;
         }
     }
 
     void StopShaking()
     {
+        Debug.Log("Happened");
         CancelInvoke("CameraShake");
-        mainCamera.transform.position = originalCameraPosition;
+       // mainCamera.transform.position = character1.transform.position;
+        Debug.Log("It seriously happened");
+       // this.gameObject.GetComponent<Collider2D>().enabled = false;
+        //this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        used = true;
+        //Invoke("Destroy", .05f);
+        //Destroy(this.gameObject);
+        //mainCamera.transform.position = originalCameraPosition;
+        /* if (MenuManager.characterSelected == 0)
+         {
+             mainCamera.transform.position = character1.transform.position;
+             Destroy(this.gameObject);
+         }
+         else if (MenuManager.characterSelected == 1)
+         {
+             mainCamera.transform.position = character2.transform.position;
+             Destroy(this.gameObject);
+         } */
+        //mainCamera.transform.position = player.position;
     }
 
 }
